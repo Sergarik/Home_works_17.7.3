3f60db6002faf25bce5d3bd00ee8d9e97f89918e
Sergarik <154251262+Sergarik@users.noreply.github.com>
2024-01-30 21:58:05 +0300

    Файл с кодом

---
+++ DZ_17.7.3.py
@@ -0,0 +1,12 @@
+# Словарь с процентными ставками
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+
+money = float(input("Введите сумму вклада: "))
+
+deposit = [int(money * rate / 100) for rate in per_cent.values()]
+
+print("Накопления за год:", deposit)
+
+max_deposit = max(deposit)
+
+print("Максимальная сумма, которую вы можете заработать —", max_deposit)
